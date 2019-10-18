f1dee8bae80848b1d0422526fb61d080fb7b6c32
David Tucker <david@tucker.name>
2019-10-17 23:45:41 -0700

    Fix cdtmp and crontab aliases

--- config/bashrc.bash
+++ config/bashrc.bash
@@ -82,8 +82,7 @@ esac
 [[ ":$PATH:" == *':.:'* ]] || export PATH=".:$PATH"
 export PIP_REQUIRE_VIRTUALENV=true
 
-alias cdtmp='cd "$(mktemp --directory)"'
-alias crontab='crontab -i'
+alias cdtmp='cd "$(mktemp -d)"'
 alias grep='grep --color=auto'
 alias less='less -r'
 alias ll='ls -h -l'
@@ -98,6 +97,7 @@ case "$(uname -s)" in
     Linux)
         eval "$(dircolors --sh)"  # Set LS_COLORS.
         alias ls='ls --color=auto'
+        alias crontab='crontab -i'
         ;;
     *) echo 'This OS is not recognized.'
 esac
