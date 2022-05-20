14286b371125c0a61179e05d88bccdbe3a70686b
David Tucker <david@tucker.name>
2022-05-19 20:09:30 -0700

    Preserve symlinked config files

--- install/vim.bash
+++ install/vim.bash
@@ -16,9 +16,9 @@ config_vimrc="$config_home/vimrc.vim"
 cp -v "$config_repo/config/$(basename "$config_vimrc")" "$config_vimrc"
 echo "rm '$config_vimrc'" >> "$config_undo"
 source_config_vimrc="source $config_vimrc"
-vimrc="$HOME/.vimrc"
+vimrc="$(realpath "$HOME/.vimrc")"
 grep -q "$source_config_vimrc" "$vimrc" || {
     echo "$source_config_vimrc" >> "$vimrc"
-    echo "sed -i.old '\|'""$(printf '%q' "$source_config_vimrc")""'|d' '$vimrc'" >> "$config_undo"
+    echo "sed -i '\|'""$(printf '%q' "$source_config_vimrc")""'|d' '$vimrc'" >> "$config_undo"
 }
 echo 'Vim configuration succeeded.'
